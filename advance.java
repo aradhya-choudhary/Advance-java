864131a9c1d64154250448225e09a329a5efb414
aradhya-choudhary <aradhyachaudhary.2001@gmail.com>
2026-02-02 22:04:53 +0530

    Add files via upload
    first commit

---
+++ advance.java
@@ -0,0 +1,12 @@
+class DisplayMessage {
+    void displayInfo() {
+        System.out.println("welcome");
+    }
+}
+
+public class advance {
+    public static void main(String[] args) {
+        DisplayMessage dm = new DisplayMessage();
+        dm.displayInfo();
+    }
+}
