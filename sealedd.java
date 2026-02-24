03a5066ddc2fbccb42557ee3a9caa0dfe768ca2e
aradhya-choudhary <aradhyachaudhary.2001@gmail.com>
2026-02-24 10:39:02 +0530

    Add files via upload

---
+++ sealedd.java
@@ -0,0 +1,16 @@
+ sealed class Shape permits Circle, Rectangle {
+
+}
+ final class Circle extends Shape{
+
+}
+non-sealed class Rectangle extends Shape {
+
+}
+ 
+
+public class sealedd {
+    public static void main(String[] args) {
+        
+    }
+}
