37b5e9ad3b83296aace0345e397172ad3cb28abd
aradhya-choudhary <aradhyachaudhary.2001@gmail.com>
2026-03-21 22:45:38 +0530

    Add files via upload

---
+++ sub.java
@@ -0,0 +1,21 @@
+public class sub {
+    public static void main(String[] args) {
+        int n =5;
+        int[] arr = {2, 3, 4, 6, 7};
+
+         for(int i=0; i<arr.length; i++){
+            for(int j=i; j<arr.length; j++){
+                for(int k =i; k<=j; k++)
+                    System.out.print(arr[k] + " ");
+                System.out.println();
+                
+         }
+         
+    }
+
+
+}
+public static void mergeSort(int[] arr, int low, int mid, int high){
+    
+}
+}
