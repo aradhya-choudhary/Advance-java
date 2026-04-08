c9b8d7b7285f68257aa4f56a8a719211d508822f
aradhya-choudhary <aradhyachaudhary.2001@gmail.com>
2026-04-08 23:09:19 +0530

    Add files via upload

---
+++ inference.java
@@ -0,0 +1,21 @@
+public class inference {
+    public static void main(String[] args) {
+        int a = 10;
+        var b= 20;
+        // var name= "aradhya";
+        var value = 10.34;
+        var c=0;
+        c=39;
+
+        String name = new String("this is string");
+        var name1 = new String("this is string with war");
+        int[] nums = {2,3,4,5,6};
+         for(var num: nums) {
+            System.out.println(num);
+        
+         }
+         int [] data = {3,3,34,2,454,54};
+         
+
+        }
+}
